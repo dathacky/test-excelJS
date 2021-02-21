878bc6fc9d91e9f60b3922af13ef5eed9cc2dd90
dat.tv <dathacky@gmail.com>
2021-02-21 14:25:13 +0700

    ...

--- src/core/excel/Excel.js
+++ src/core/excel/Excel.js
@@ -16,16 +16,19 @@ class Excel {
   async importProject(file) {
     await this.workbook.xlsx.readFile(file);
     const worksheetSumary = this.workbook.getWorksheet(DEFINE.SUMARY.WORKSHEET);
-    const name = worksheetSumary.getCell(DEFINE.SUMARY.NAME);
-    const created_by = worksheetSumary.getCell(DEFINE.SUMARY.CREATED_BY);
-    const created_at = worksheetSumary.getCell(DEFINE.SUMARY.CREATED_AT);
-    const description = worksheetSumary.getCell(DEFINE.SUMARY.DESCRIPTION);
-    this._sumary = {
-      name,
-      created_by,
-      created_at,
-      description,
-    };
+    // TODO: throw if haven't worksheet?
+    if (worksheetSumary) {
+      const name = worksheetSumary.getCell(DEFINE.SUMARY.NAME);
+      const created_by = worksheetSumary.getCell(DEFINE.SUMARY.CREATED_BY);
+      const created_at = worksheetSumary.getCell(DEFINE.SUMARY.CREATED_AT);
+      const description = worksheetSumary.getCell(DEFINE.SUMARY.DESCRIPTION);
+      this._sumary = {
+        name,
+        created_by,
+        created_at,
+        description,
+      };
+    }
   }
 
   async exportProject(file) {
@@ -36,50 +39,57 @@ class Excel {
     const worksheetTestcases = this.workbook.getWorksheet(
       DEFINE.TESTCASES.WORKSHEET,
     );
-    worksheetTestcases
-      .getColumn(DEFINE.TESTCASES.NAME_WORKSHEET)
-      .eachCell({ includeEmpty: false }, (cell) => {
-        const nameTestcase = cell.value;
-        this.addDataValidationTestCase(nameTestcase);
-      });
+    // TODO: throw if haven't worksheet?
+    if (worksheetTestcases) {
+      worksheetTestcases
+        .getColumn(DEFINE.TESTCASES.NAME_WORKSHEET)
+        .eachCell({ includeEmpty: false }, (cell) => {
+          const nameTestcase = cell.value;
+          this.addDataValidationTestCase(nameTestcase);
+        });
+    }
   }
 
   addDataValidationTestCase(worksheetName) {
-    //TODO validation input, description, check exist worksheet
+    // TODO validation input, description
     const worksheetTestcase = this.workbook.getWorksheet(worksheetName);
-    const options = values(DEFINE.TESTCASE.LIST_ACTION).join(',');
-    worksheetTestcase
-      .getColumn(DEFINE.TESTCASE.COLUMN_TYPE_ACTION)
-      .eachCell({ includeEmpty: true }, (cell) => {
-        cell.dataValidation = {
-          type: 'list',
-          allowBlank: false,
-          formulae: [`"${options}"`],
-          operator: 'equal',
-          showErrorMessage: true,
-          errorStyle: 'error',
-          errorTitle: 'Invalid action',
-          error: 'The action invalid, must be the value in the list',
-        };
-      });
+    if (worksheetTestcase) {
+      const options = values(DEFINE.TESTCASE.LIST_ACTION).join(',');
+      worksheetTestcase
+        .getColumn(DEFINE.TESTCASE.COLUMN_TYPE_ACTION)
+        .eachCell({ includeEmpty: true }, (cell) => {
+          cell.dataValidation = {
+            type: 'list',
+            allowBlank: false,
+            formulae: [`"${options}"`],
+            operator: 'equal',
+            showErrorMessage: true,
+            errorStyle: 'error',
+            errorTitle: 'Invalid action',
+            error: 'The action invalid, must be the value in the list',
+          };
+        });
+    }
   }
 
   loadScript(scriptName) {
     const scripts = [];
     let invalid = [];
     const worksheetTestcases = this.workbook.getWorksheet(scriptName);
-    worksheetTestcases
-      .getColumn(DEFINE.REPOSITORIES.NAME_WORKSHEET)
-      .eachCell({ includeEmpty: true }, (cell) => {
-        const nameWorksheetTestcase = cell.value;
-        const resultScript = this.getScriptActionFromTestcase(
-          nameWorksheetTestcase,
-        );
-        if (!resultScript.isValid) {
-          invalid.push(nameWorksheetTestcase);
-        }
-        scripts.push(...resultScript.actions);
-      });
+    if (worksheetTestcases) {
+      worksheetTestcases
+        .getColumn(DEFINE.REPOSITORIES.NAME_WORKSHEET)
+        .eachCell({ includeEmpty: true }, (cell) => {
+          const nameWorksheetTestcase = cell.value;
+          const resultScript = this.getScriptActionFromTestcase(
+            nameWorksheetTestcase,
+          );
+          if (!resultScript.isValid) {
+            invalid.push(nameWorksheetTestcase);
+          }
+          scripts.push(...resultScript.actions);
+        });
+    }
     return { scripts, invalid };
   }
 
@@ -87,41 +97,45 @@ class Excel {
     const actions = [];
     let valid = true;
     const worksheetTestcase = this.workbook.getWorksheet(testcaseName);
-    worksheetTestcase.eachRow({ includeEmpty: false }, (cell) => {
-      const numberCell = get(cell, 'number');
-      const testcaseName = get(cell, 'worksheet.name');
-      const modelCells = get(cell, 'model.cells');
-      const typeActionCell = find(modelCells, {
-        address: DEFINE.TESTCASE.COLUMN_TYPE_ACTION + numberCell,
+    if (worksheetTestcase) {
+      worksheetTestcase.eachRow({ includeEmpty: false }, (cell) => {
+        const numberCell = get(cell, 'number');
+        const testcaseName = get(cell, 'worksheet.name');
+        const modelCells = get(cell, 'model.cells');
+        const typeActionCell = find(modelCells, {
+          address: DEFINE.TESTCASE.COLUMN_TYPE_ACTION + numberCell,
+        });
+        const xpathCell = find(modelCells, {
+          address: DEFINE.TESTCASE.COLUMN_XPATH + numberCell,
+        });
+        const inputCell = find(modelCells, {
+          address: DEFINE.TESTCASE.COLUMN_INPUT + numberCell,
+        });
+        const descriptionCell = find(modelCells, {
+          address: DEFINE.TESTCASE.COLUMN_DESCRIPTION + numberCell,
+        });
+        const typeAction = get(typeActionCell, 'value');
+        const xpath = get(xpathCell, 'value') || get(xpathCell, 'text');
+        const input = get(inputCell, 'value') || get(inputCell, 'text');
+        const description =
+          get(descriptionCell, 'value') || get(descriptionCell, 'text');
+        const isValid = this.validateAction(typeAction, input);
+        if (!isValid) {
+          valid = false;
+        }
+        const dataScript = {
+          testcaseName,
+          typeAction,
+          xpath,
+          input,
+          description,
+          isValid,
+        };
+        actions.push(dataScript);
       });
-      const xpathCell = find(modelCells, {
-        address: DEFINE.TESTCASE.COLUMN_XPATH + numberCell,
-      });
-      const inputCell = find(modelCells, {
-        address: DEFINE.TESTCASE.COLUMN_INPUT + numberCell,
-      });
-      const descriptionCell = find(modelCells, {
-        address: DEFINE.TESTCASE.COLUMN_DESCRIPTION + numberCell,
-      });
-      const typeAction = get(typeActionCell, 'value');
-      const xpath = get(xpathCell, 'value') || get(xpathCell, 'text');
-      const input = get(inputCell, 'value') || get(inputCell, 'text');
-      const description =
-        get(descriptionCell, 'value') || get(descriptionCell, 'text');
-      const isValid = this.validateAction(typeAction, input);
-      if (!isValid) {
-        valid = false;
-      }
-      const dataScript = {
-        testcaseName,
-        typeAction,
-        xpath,
-        input,
-        description,
-        isValid,
-      };
-      actions.push(dataScript);
-    });
+    } else {
+      valid = false;
+    }
     return { actions, isValid: valid };
   }
 
